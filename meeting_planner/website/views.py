a910f505beb3e64ff91e75d56171455474fb08a2
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2020-06-21 09:36:48 +0300

    Add date view

--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -1,6 +1,12 @@
+from datetime import datetime
+
 from django.shortcuts import render
 from django.http import HttpResponse
 
 
 def welcome(request):
     return HttpResponse("Welcome to meeting planner!")
+
+
+def date(request):
+    return HttpResponse(f"This page was server at {datetime.now()}")
