46ff87a63ab53d4f82b94531fec6c708aeffacb7
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2020-06-22 16:37:36 +0300

    Add Meeting => Room fk

--- meeting_planner/meetings/models.py
+++ meeting_planner/meetings/models.py
@@ -16,6 +16,7 @@ class Meeting(models.Model):
     date = models.DateField()
     start_time = models.TimeField(default=time(9))
     duration = models.IntegerField(default=1)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.title} at {self.start_time} on {self.date}"
