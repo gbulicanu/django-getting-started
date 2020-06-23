2319a5b4714d0014be3029df1f901f8ff74229c0
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2020-06-23 09:01:35 +0300

    Implement MTV for welcome view

--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -3,11 +3,13 @@ from datetime import datetime
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from meetings.models import Meeting
+
 
 def welcome(request):
     return render(request,
                   "website/welcome.html",
-                  {"message": "This data was sent from view to the template."})
+                  {"num_meetings": Meeting.objects.count()})
 
 
 def date(request):
